81d775b8000294304d5f06560008396d46fbfd25
dresson <edresson1@gmail.com>
2019-06-03 21:13:21 -0300

    inicial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+DEBUG = True
+#mongodb ip
+DB_HOST = '127.0.0.1'
+#mongodb port
+DB_PORT = 27017
+#chave para geração de token
+KEY = 'chaveHS256'
+#tempo em dias para expirar o token de ativação da conta
+ACTIVATION_EXPIRE_DAYS = 5
+#tempo em horas para expirar o token de login
+TOKEN_EXPIRE_HOURS = 1
